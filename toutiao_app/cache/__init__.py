c6e35d93961b36a16907b7bfbbbb4d85b93ffe16
王向伟 <hnzzxiaowang@foxmail.com>
2026-05-23 15:01:23 +0800

    新闻分类和新闻列表加入缓存策略

---
+++ toutiao_app/cache/__init__.py
@@ -0,0 +1,17 @@
+# 导入新闻缓存操作函数
+from toutiao_app.cache.news_cache import (
+    get_categories_from_cache,
+    set_categories_to_cache,
+    get_news_list_from_cache,
+    set_news_list_to_cache,
+    clear_news_cache,
+)
+
+# 导出所有缓存函数
+__all__ = [
+    "get_categories_from_cache",
+    "set_categories_to_cache",
+    "get_news_list_from_cache",
+    "set_news_list_to_cache",
+    "clear_news_cache",
+]
